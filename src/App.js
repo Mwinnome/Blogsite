12b3e289ce9288b8146647d7c1a5a7eee5f3631e
Mwinnome <101268966+Mwinnome@users.noreply.github.com>
2022-04-01 08:05:46 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,12 +3,15 @@ import Home from './pages/Home'
 import { Routes, Route, Navigate } from 'react-router-dom'
 import Blog from './pages/Blog'
 import About from './pages/About'
+import BlogList from "./pages/Blog/blogs"
 
 function App() {
   return (
     <Routes>
     <Route path="/" element={<Home/>} />
-    <Route path='/blog' element={<Blog/>}/>
+    <Route path='/blog' element={<Blog/>}>
+    <Route index element={<BlogList/>}/>
+    </Route>
     <Route path='/about' element={<About/>}/>
     <Route path='*' element={<Navigate to="/"/>}/>
     
